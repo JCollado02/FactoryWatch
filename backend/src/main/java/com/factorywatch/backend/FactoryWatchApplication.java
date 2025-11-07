72f01e81ed8e4636c065862398a5b147c5b4ec30
JCollado02 <ji.sito122@gmail.com>
2025-11-06 20:29:19 -0500

    Added Spring Boot backend starter

---
+++ backend/src/main/java/com/factorywatch/backend/FactoryWatchApplication.java
@@ -0,0 +1,13 @@
+package com.factorywatch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryWatchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/factorywatch/backend/FactoryWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factorywatch.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
