944d2208ae16812de71b3549adfbff95814d3e0d
JCollado02 <ji.sito122@gmail.com>
2025-11-13 12:55:48 -0500

    added method for finding by device ID

--- backend/src/main/java/com/factorywatch/backend/repository/SensorReadingRepository.java
+++ backend/src/main/java/com/factorywatch/backend/repository/SensorReadingRepository.java
@@ -1,5 +1,7 @@
 package com.factorywatch.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.factorywatch.backend.model.SensorReading;
 
 @Repository
 public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
-
+    List<SensorReading> findByDeviceId(String deviceId);
 }
